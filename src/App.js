bb77cd20de24e270eb81273d164bedc98bb684f1
Saj-Das <sajald1993@gmail.com>
2021-01-19 20:42:13 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from "react";
 import "./style.css";
-import VideoCardLayout from './Components/VideoCardLayout.js'
+import VideoCardLayout from "./Components/VideoCardLayout.js";
+import SearchBar from "./Components/SearchBar.js";
 export default function App() {
   return (
-    <div>
-      <VideoCardLayout />
+    <div class="row">
+      <div class="col-md-6">
+        <VideoCardLayout />
+      </div>
+      <div class="col-md-6">
+        <SearchBar />
+      </div>
     </div>
   );
 }
