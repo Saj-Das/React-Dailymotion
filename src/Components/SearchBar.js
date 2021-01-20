8e90094fe2436c25d80dde6eb6bf74e6dac75099
Saj-Das <sajald1993@gmail.com>
2021-01-20 18:57:51 +0530

    1 file modified

--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -1,40 +1,46 @@
-import React from 'react'
+import React from "react";
 
-class SearchBar extends React.Component{
-
+class SearchBar extends React.Component {
   constructor() {
-        super()  
-        this.state={searchText:null};    
-        this.handleChange = this.handleChange.bind(this);
-    }
+    super();
+    this.state = { searchText: null };
+    this.handleChange = this.handleChange.bind(this);
+  }
 
- handleChange({ target }) {
+  handleChange({ target }) {
     this.setState({
       [target.name]: target.value
     });
   }
-    render()
-    {
-      return (
-        
-        <div class="input-group mb-3">
-  <input type="text" class="form-control" 
-  value={this.state.searchText}  onChange={ this.handleChange }  name="searchText" id="searchText"
-  aria-label="search" aria-describedby="basic-addon2" />
-  <div class="input-group-append">
-    <button class="btn btn-outline-secondary" onClick={(e) => this.search()}
-    type="button">Search</button>
-  </div>
-</div>
-      )  
-        
-        
-    }
-
-    search()
-    {
-      console.log("searchText",this.state.searchText)
-    }
+  render() {
+    return (
+      <div class="input-group mb-3">
+        <input
+          type="text"
+          class="form-control"
+          value={this.state.searchText}
+          onChange={this.handleChange}
+          name="searchText"
+          id="searchText"
+          aria-label="search"
+          aria-describedby="basic-addon2"
+        />
+        <div class="input-group-append">
+          <button
+            class="btn btn-outline-secondary"
+            onClick={e => this.search()}
+            type="button"
+          >
+            Search
+          </button>
+        </div>
+      </div>
+    );
   }
 
-  export default SearchBar;
+  search() {
+    console.log("searchText", this.state.searchText);
+  }
+}
+
+export default SearchBar;
