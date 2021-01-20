2ddbe61d99878ffa68d88121b46b8347a20042a0
Saj-Das <sajald1993@gmail.com>
2021-01-20 17:47:00 +0530

    1 file modified

--- src/Components/VideoCardLayout.js
+++ src/Components/VideoCardLayout.js
@@ -11,10 +11,10 @@ class VideoCardLayout extends React.Component {
   render() {
     const videos = this.state.videoList.map(item => (
       <div class="col-md-3" key={item.id}>
-        <img src={item.thumbnail_url} style={{ width: 220 + "px" }} />
-        <h4>{item.title}</h4>
-        <h4>{item.views_total}</h4>
-        <h4>{item.duration}</h4>
+        <img src={item.thumbnail_url} style={{ width: 180 + "px" }} />
+        <h7>{item.title}</h7>
+        <h7>{item.views_total}</h7>
+        <h7>{item.duration}</h7>
       </div>
     ));
 
