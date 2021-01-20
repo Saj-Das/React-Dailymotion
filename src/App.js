938137ed1c01f2cd41a394755308a095e017eaf8
Saj-Das <sajald1993@gmail.com>
2021-01-20 18:59:12 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -4,12 +4,12 @@ import VideoCardLayout from "./Components/VideoCardLayout.js";
 import SearchBar from "./Components/SearchBar.js";
 export default function App() {
   return (
-    <div class="row" style={{padding: 20+"px"}}>
+    <div class="row" style={{ padding: 20 + "px" }}>
       <div class="col-md-2">
-         <SearchBar />
+        <SearchBar />
       </div>
       <div class="col-md-10">
-      <VideoCardLayout />
+        <VideoCardLayout />
       </div>
     </div>
   );
