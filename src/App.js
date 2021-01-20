44e24687aa58db0b3bf5e1eac82b97fe50eaadec
Saj-Das <sajald1993@gmail.com>
2021-01-20 17:53:29 +0530

    2 files modified

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import VideoCardLayout from "./Components/VideoCardLayout.js";
 import SearchBar from "./Components/SearchBar.js";
 export default function App() {
   return (
-    <div class="row">
+    <div class="row" style={{padding: 20+"px"}}>
       <div class="col-md-9">
         <VideoCardLayout />
       </div>
--- src/Components/VideoCardLayout.js
+++ src/Components/VideoCardLayout.js
@@ -11,10 +11,10 @@ class VideoCardLayout extends React.Component {
   render() {
     const videos = this.state.videoList.map(item => (
       <div class="col-md-3" key={item.id}>
-        <img src={item.thumbnail_url} style={{ width: 180 + "px" }} />
+        <img src={item.thumbnail_url} style={{ width: 220 + "px" }} />
        <br/> <h7>{item.title}</h7>
-       <br/> <h7>{item.views_total}</h7>
-       <br/> <h7>{item.duration}</h7>
+       <br/>Views: <h7>{item.views_total}</h7>
+       <br/>Duration: <h7>{item.duration}</h7>
       </div>
     ));
 
