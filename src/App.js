85595aad20e1916f66be2dee699957ac4e3293b3
Saj-Das <sajald1993@gmail.com>
2021-01-21 22:27:57 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -2,15 +2,19 @@ import React from "react";
 import "./style.css";
 import VideoCardLayout from "./Components/VideoCardLayout.js";
 import SearchBar from "./Components/SearchBar.js";
-export default function App() {
-  return (
-    <div class="row" style={{ padding: 20 + "px" }}>
-      <div class="col-md-2">
-        <SearchBar />
+
+class App extends React.Component {
+  render() {
+    return (
+      <div class="row" style={{ padding: 20 + "px" }}>
+        <div class="col-md-2">
+          <SearchBar updateFilter={this.handleFilterUpdate} />
+        </div>
+        <div class="col-md-10">
+          <VideoCardLayout />
+        </div>
       </div>
-      <div class="col-md-10">
-        <VideoCardLayout />
-      </div>
-    </div>
-  );
+    );
+  }
 }
+export default App;
