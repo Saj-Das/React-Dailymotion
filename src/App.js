98d9185f277fcecfcc86ddf051ad9917d1e488b0
Saj-Das <sajald1993@gmail.com>
2021-01-30 10:12:11 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 import React from "react";
 import "./style.css";
 import VideoDashboard from "./Components/VideoDashboard.js";
-import VideoPreview from "./Components/VideoDashboard.js";
+import VideoPreview from "./Components/VideoPreview.js";
 import { Link, BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 class App extends React.Component {
   render() {
     return (
-     <Router>
-      <div>
-        <Switch>
-          <Route exact path="/" component={VideoDashboard} />
-          <Route exact path="/preview" component={VideoPreview}/>
-        </Switch>
-      </div>
-    </Router>
+      <Router>
+        <div>
+          <Switch>
+            <Route exact path="/" component={VideoDashboard} />
+            <Route exact path="/preview" component={VideoPreview} />
+          </Switch>
+        </div>
+      </Router>
     );
   }
 }
