2728691226566016d2f73087b16fb16285f5d552
Saj-Das <sajald1993@gmail.com>
2021-01-30 10:21:41 +0530

    1 file modified

--- src/Components/VideoPreview.js
+++ src/Components/VideoPreview.js
@@ -3,8 +3,14 @@ import ReactDom from "react-dom";
 
 class VideoPreview extends React.Component {
   render() {
-    return <div>asdasd</div>;
+    return <div>Preview</div>;
   }
+  
+  componentDidMount() {
+   const id=this.props.match.params.id;
+   console.log(this.props.match.params.id)
+  }
+
 }
 
 export default VideoPreview;
